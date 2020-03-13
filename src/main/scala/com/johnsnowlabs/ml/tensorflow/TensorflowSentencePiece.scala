ef3994a46a67ec503a85b6b0aa54aba386b555ca
C-K-Loan <christian.Kasim.loan@gmail.com>
2020-03-13 01:18:55 +0100

    Integrated Sentence Piece Tokens to String Output and integrated it to XLNetEmbeddings

--- src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowSentencePiece.scala
+++ src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowSentencePiece.scala
@@ -23,21 +23,23 @@ import scala.collection.Map
 
 class TensorflowSentencePiece(val tensorflow: TensorflowWrapper,
                               batchSize: Int,
-                              configProtoBytes: Option[Array[Byte]] = None
+                              configProtoBytes: Option[Array[Byte]] = None,
+                              EOSToken: String = "[TODO]"
                              ) extends Serializable {
 
 
   /**
     * Calculate the embeddings for a sequence of Tokens and create WordPieceEmbeddingsSentence objects from them
+    * It corrosponds to the encode() method of the tf_sentencepiece module
     *
     * @param sentences A sequence of Tokenized Sentences for which embeddings will be calculated
     * @return A Seq of WordpieceEmbeddingsSentence, one element for each input sentence
     */
-  def calculateTokensAndIds(sentences: Seq[String]): Seq[Annotation] = {
+  def calculateTokensAndIds(sentences: Seq[String]): Seq[WordpieceTokenizedSentence] = {
 
     /*Run embeddings calculation by batches*/
     //    sentences.zipWithIndex.grouped(batchSize).flatMap { batch =>
-    val vectors = calculateIds(sentences)
+    val vectors = tokenizeAndGetIds(sentences)
 
     //        }
     /*Combine tokens and sentences  and their calculated tokens and IDs TODO*/
@@ -46,12 +48,13 @@ class TensorflowSentencePiece(val tensorflow: TensorflowWrapper,
   }
 
   /**
-    * This function will tokenize each sentence and find the ID's for each of the tokens.
+    * This function will tokenize each sentence and find the ID's for each of the tokens using a Sentence Piece model.
+    * It corrosponds to the piece_to_id() method of the tf_sentencepiece module
     *
     * @param batch The sentences for which we calculate tokens and ids
     * @return The Embeddings Vector. For each Seq Element we have a Sentence, and for each sentence we have an Array for each of its words. Each of its words gets a float array to represent its Embeddings
     */
-  def calculateIds(batch: Seq[String]): Seq[Annotation] = {
+  def tokenizeAndGetIds(batch: Seq[String]): Seq[WordpieceTokenizedSentence] = {
 
     val tensors = new TensorResources()
 
@@ -67,34 +70,100 @@ class TensorflowSentencePiece(val tensorflow: TensorflowWrapper,
     runner
       .feed("sentenceInput", sentenceTensors)
       .fetch("ids")
-      .fetch("seq_len")
+    //      .fetch("seq_len")  we can remove ths  by just getting the length in java..
 
 
     val outs = runner.run().asScala
     val ids = TensorResources.extractInts(outs(0)) // Depends on the fetch order!
-    val seq_lens = TensorResources.extractInts(outs(1))
+    //    val seq_lens = TensorResources.extractInts(outs(1))
 
     print("debug")
 
     tensors.clearSession(outs)
     tensors.clearTensors()
+    WordpieceTokenizedSentence
 
-    batch.zip(ids).map { case (sentence, id) => // TODO seq len zip
-      Annotation(
-        annotatorType = AnnotatorType.SENTENCE_EMBEDDINGS,
-        begin = -1, //sentence.start,
-        end = 1, // sentence.end,
-        result = ids.mkString(","),
-        metadata = Map("sentence" -> sentence,
-          "pieceId" -> "-1",
-          "containsOOV" -> "false" //todo
-        ),
-        embeddings = Array[Float]()
-      )
+    batch.zip(ids).map { case (sentence, id) =>
+      Array(TokenPiece(wordpiece = ids.mkString(","), //inefficient hack
+        token = "#",
+        pieceId = -1,
+        isWordStart = true,
+        begin = -1,
+        end = -1
+      ))
 
-    }
+    }.map(tokens => WordpieceTokenizedSentence(tokens))
 
   }
+
+
+  /**
+    * This function will tokenize each sentence and find the ID's for each of the tokens using a Sentence Piece model.
+    *
+    * @param batch The sentences Ids for which we calculate original string tokens
+    * @return The Embeddings Vector. For each Seq Element we have a Sentence, and for each sentence we have an Array for each of its words. Each of its words gets a float array to represent its Embeddings
+    */
+  def getTokensForIds(batch: Seq[Seq[Int]]): Seq[WordpieceTokenizedSentence] = {
+    // this could be done just using a dict..
+    val tensors = new TensorResources()
+
+    /* Actual size of each sentence to skip padding in the TF model */
+
+    val sentencesBytes = batch.map { sentence =>
+      sentence.map(id => id).toArray
+    }.toArray
+
+    val sentenceTensors = tensors.createTensor(sentencesBytes)
+    val runner = tensorflow.getSession(configProtoBytes = configProtoBytes).runner
+
+    runner
+      .feed("tokensInput", sentenceTensors)
+      .fetch("tokensOut")
+    //      .fetch("seq_len")  we can remove ths  by just getting the length in java..
+
+
+    val outs = runner.run().asScala
+    val tokensBytes = TensorResources.extractBytes(outs(0)) // Depends on the fetch order!
+    //    val seq_lens = TensorResources.extractInts(outs(1))
+    val string = new String(tokensBytes, "UTF-8");
+
+    print("debug")
+
+    tensors.clearSession(outs)
+    tensors.clearTensors()
+    WordpieceTokenizedSentence
+
+    val res = batch.zip(Seq(tokensBytes)).map { case (tokenId, tokenBytes) =>
+      Array(TokenPiece(wordpiece = new String(tokensBytes, "UTF-8"), // "lol".mkString(","), //inefficient hack
+        token = batch(0).mkString(","),
+        pieceId = -1,
+        isWordStart = true,
+        begin = -1,
+        end = -1
+      ))
+
+    }.map(tokens => WordpieceTokenizedSentence(tokens))
+    print(res)
+    res
+  }
+
+  // OLD ANNOTATION BASED IMPLEMENTATION below
+  //  batch.zip(ids).map { case (sentence, id) => // TODO seq len zip
+  //    Annotation(
+  //      annotatorType = AnnotatorType.SENTENCE_EMBEDDINGS,
+  //      begin = -1, //sentence.start,
+  //      end = -1, // sentence.end,
+  //      result = ids.mkString(","),
+  //      metadata = Map("sentence" -> sentence,
+  //        "pieceId" -> "-1",
+  //        "containsOOV" -> "false", //todo
+  //        "sequence_length" -> ids.length.toString
+  //      ),
+  //      embeddings = Array[Float]()
+  //    )
+  //
+  //  }
+
 
   //  /**
   //    * This function will find the strings for the ID's calculated by the sentencePiece model.
@@ -151,28 +220,4 @@ class TensorflowSentencePiece(val tensorflow: TensorflowWrapper,
   //
   //  }
 
-
-  /**
-    * word_emb: the character-based word representations with shape [batch_size, max_length, 512].  == 512
-    * lstm_outputs1: the first LSTM hidden state with shape [batch_size, max_length, 1024]. === 1024
-    * lstm_outputs2: the second LSTM hidden state with shape [batch_size, max_length, 1024]. === 1024
-    * elmo: the weighted sum of the 3 layers, where the weights are trainable. This tensor has shape [batch_size, max_length, 1024]  == 1024
-    *
-    * @param layer Layer specification
-    * @return The dimension of chosen layer
-    */
-  def getDimensions(layer: String): Int = {
-    {
-      layer match {
-        case "word_emb" =>
-          512
-        case "lstm_outputs1" =>
-          1024
-        case "lstm_outputs2" =>
-          1024
-        case "elmo" =>
-          1024
-      }
-    }
-  }
 }
